bae2fed7b5c7ee6a59e50e199c4195c968878301
Igor Bonadio <igorbonadio@gmail.com>
2015-03-27 17:50:37 -0300

    Add cons list (with persistence)

--- src/list.rs
+++ src/list.rs
@@ -1,15 +1,13 @@
+use std::rc::Rc;
+
 pub enum List<T> {
-    Cons(T, Box<List<T>>),
+    Cons(T, Rc<List<T>>),
     Empty
 }
 
 impl<T> List<T> {
-    pub fn empty() -> List<T> {
-        List::Empty
-    }
-
-    pub fn prepend(self, value: T) -> List<T> {
-        List::Cons(value, Box::new(self))
+    pub fn empty() -> Rc<List<T>> {
+        Rc::new(List::Empty)
     }
 
     pub fn len(&self) -> usize {
@@ -20,11 +18,17 @@ impl<T> List<T> {
     }
 }
 
+pub fn prepend<T>(list: Rc<List<T>>, value: T) -> Rc<List<T>> {
+    Rc::new(List::Cons(value, list))
+}
+
 #[test]
 fn test_len() {
-    let x = List::empty().prepend(1).prepend(2).prepend(3);
-    assert!(x.len() == 3);
+    let x = prepend(prepend(prepend(List::empty(), 1), 2), 3);
+    let yx = prepend(x.clone(), 4);
+    let zx = prepend(x.clone(), 5);
 
-    let y = x.prepend(4);
-    assert!(y.len() == 4);
+    assert!(x.len() == 3);
+    assert!(yx.len() == 4);
+    assert!(zx.len() == 4);
 }
